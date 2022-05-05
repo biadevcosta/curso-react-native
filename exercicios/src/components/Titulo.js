f226141d9f1dbf2df82c24be120bab4e09554e0b
Ana Beatriz Costa <contato.anacosta2@gmail.com>
2022-05-05 14:23:39 -0300

    React fragment <> </>

--- exercicios/src/components/Titulo.js
+++ exercicios/src/components/Titulo.js
@@ -1,12 +1,12 @@
-import React, {Fragment} from "react"
+import React from "react"
 import { View, Text} from 'react-native'
 import estilo from './estilo'
 
 export default props => {
     return (
-        <Fragment>
+        <>
             <Text style= {estilo.txtG}> {props.principal}</Text>
             <Text style= {estilo.txtP}> {props.secundario}</Text>
-        </Fragment>
+        </>
     )
 }
