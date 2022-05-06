6e644abfabe7102cf6e1db4edaa492b98108a85e
Ana Beatriz Costa <contato.anacosta2@gmail.com>
2022-05-06 15:16:28 -0300

    funcoes

--- exercicios/src/components/Botao.js
+++ exercicios/src/components/Botao.js
@@ -3,10 +3,26 @@ import { Button } from "react-native"
 
 export default props =>  {
     function executar (){
-        console.warn ("Exec!!")
+        console.warn ("Exec 01!!")
     }
     return(
-        <Button title="Executar!" onPress={executar}/>
+        <>
+        
+        <Button
+         title="Executar 01" 
+         onPress={executar}/>
 
+        <Button
+        title="Executar 02"
+        onPress= {function (){
+            console.warn('Exec 02')
+        }}
+        />
+
+        <Button
+        title="Executar 03"
+        onPress={() => console.warn('Exec 03')}
+        />
+        </>
     )
 }
